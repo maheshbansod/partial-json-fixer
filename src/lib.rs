c7c9330561bf73a5e0ee59076177397b8266e465
Mahesh Bansod <coolkevin011@gmail.com>
2024-11-21 23:47:19 +0530

    update doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
+//! Partial JSON fixer
+//! 
+//! This is a zero dependency partial json fixer.
+//! It is very lenient, and will accept some erroneous JSON too. For example, {key: "value"} would be valid.
+//! 
+//! This can be used to parse partial json coming from a stream.
+
 use std::str::CharIndices;
 
+/// Takes a partial JSOn string and returns a complete JSON string
 pub fn fix_json(partial_json: &str) -> JResult<String> {
     if partial_json.len() == 0 {
         return Ok("".to_string())
