cc2a0bffb58f8b195df7693d9b640212921d4210
Mahesh Bansod <coolkevin011@gmail.com>
2024-11-22 10:00:57 +0530

    integrate rs

--- py/partial-json-fixer/src/lib.rs
+++ py/partial-json-fixer/src/lib.rs
@@ -1,14 +1,16 @@
-use pyo3::prelude::*;
+use original_partial_json_fixer;
+use pyo3::{exceptions::PyValueError, prelude::*};
 
-/// Formats the sum of two numbers as string.
+/// Fixes partial json to return a complete json string
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn fix_json(partial_json: &str) -> PyResult<String> {
+    Ok(original_partial_json_fixer::fix_json(partial_json)
+        .map_err(|err| PyValueError::new_err(err.to_string()))?)
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn partial_json_fixer(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(fix_json, m)?)?;
     Ok(())
 }
